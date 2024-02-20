9cbfaa584dab3ca771d6feb8cab1712b7ad7b34a
zeusyosefgithub <midligyosef190@gmail.com>
2024-02-20 14:05:47 +0200

    aading fix

--- app/api/route.js
+++ app/api/route.js
@@ -69,20 +69,20 @@ export async function POST(request) {
     //     });
 
 
-    const qrcode = require('qrcode-terminal');
+    // const qrcode = require('qrcode-terminal');
 
-    const { Client } = require('whatsapp-web.js');
-    const client = new Client();
+    // const { Client } = require('whatsapp-web.js');
+    // const client = new Client();
     
-    client.on('qr', (qr) => {
-        qrcode.generate(qr, { small: true });
-    });
+    // client.on('qr', (qr) => {
+    //     qrcode.generate(qr, { small: true });
+    // });
     
-    client.on('ready', () => {
-        console.log('Client is ready!');
-    });
+    // client.on('ready', () => {
+    //     console.log('Client is ready!');
+    // });
     
-    client.initialize();
+    // client.initialize();
      
      
 
