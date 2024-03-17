81f0b10aba834e5adec7da7fccc55a4a0b3f5ce6
zeusyosefgithub <midligyosef190@gmail.com>
2024-03-17 02:22:40 +0200

    fixing quality image

--- app/Componenets/CustomerLogin.js
+++ app/Componenets/CustomerLogin.js
@@ -1,5 +1,5 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Button } from "@nextui-org/react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import rep6 from "../../images/rep6.png";
 import Link from "next/link";
 import { GrUpdate } from "react-icons/gr";
@@ -11,6 +11,8 @@ import LogOut from "./logout";
 import GetTrucks from "./getDocs";
 import { FiArrowRight } from "react-icons/fi";
 import { ComponentToPrint } from "./toPrint";
+import html2canvas from "html2canvas";
+import { Invoiceimage } from "./Invoiceimage";
 
 
 export default function CustomerLogin(props) {
@@ -25,7 +27,7 @@ export default function CustomerLogin(props) {
     ];
 
 
-    function getAllPropDate(date){
+    function getAllPropDate(date) {
         let day = "";
         let month = "";
         let year = "";
@@ -35,33 +37,33 @@ export default function CustomerLogin(props) {
         let count3 = 0;
 
         for (let index = 0; index < date.length; index++) {
-            if(date[index] == "/"){
+            if (date[index] == "/") {
                 count1++;
             }
-            else if(count1 == 0){
+            else if (count1 == 0) {
                 day += date[index];
             }
         }
         for (let index = 0; index < date.length; index++) {
-            if(date[index] == "/"){
+            if (date[index] == "/") {
                 count2++;
             }
-            else if(count2 == 1){
+            else if (count2 == 1) {
                 month += date[index];
             }
         }
         for (let index = 0; index < date.length; index++) {
-            if(date[index] == "/"){
+            if (date[index] == "/") {
                 count3++;
             }
-            else if(count3 == 2){
+            else if (count3 == 2) {
                 year += date[index];
             }
         }
-        return {day : parseFloat(day),month : parseFloat(month),year : parseFloat(year)}
+        return { day: parseFloat(day), month: parseFloat(month), year: parseFloat(year) }
     }
 
-    function sortDateInvoice(a,b) {
+    function sortDateInvoice(a, b) {
         let bb = `${getAllPropDate(b.invoices_data)?.year}-${getAllPropDate(b.invoices_data)?.month}-${getAllPropDate(b.invoices_data)?.day}`;
         let aa = `${getAllPropDate(a.invoices_data)?.year}-${getAllPropDate(a.invoices_data)?.month}-${getAllPropDate(a.invoices_data)?.day}`;
         return new Date(bb) - new Date(aa);
@@ -86,43 +88,66 @@ export default function CustomerLogin(props) {
     const [invoice, setInvoice] = useState(null);
     const [showInvoice, setShowInvoice] = useState(false);
 
+    const downloadImageInvoice = async (imageFileName) => {
+        const canvasss = await html2canvas(reff.current,{ scale: 2 }).then(function (canvas){
+            const image = canvas.toDataURL("image/png", 1.0);
+            downloadImage(image, imageFileName);
+        });
+    }
+
+    const downloadImage = (blob, fileName) => {
+        const fakeLink = window.document.createElement("a");
+        fakeLink.style = "display:none;";
+        fakeLink.download = fileName;
+        fakeLink.href = blob;
+        document.body.appendChild(fakeLink);
+        fakeLink.click();
+        document.body.removeChild(fakeLink);
+        fakeLink.remove();
+    };
+
+    const reff = useRef();
+
     return (
         <div className="flex justify-center">
             <div className="mt-20">
 
                 {
                     showInvoice ?
-                    <div>
-                        <div className="flex ">
-                            <Button onClick={() => {setInvoice(null);setShowInvoice(null);}}>الصفحة السابقة<FiArrowRight/></Button>
-                        </div>
                         <div>
-                            {
-                                invoice.shippings.map((ship,index) => {
-                                    return <div className="w-fit">
-                                        <ComponentToPrint isLocated={ship?.location ? true : false} languge={true} currentTruck={index + 1} currentQuan={ship?.current_quantity} shippingList={ship} inewInv={invoice}/>
-                                    </div>
-                                })
-                            }
+                            <div className="flex">
+                                <Button onClick={() => { setInvoice(null); setShowInvoice(null); }}>الصفحة السابقة<FiArrowRight /></Button>
+                            </div>
+                            <div>
+                                {
+                                    invoice.shippings.map((ship, index) => {
+                                        return <div>
+                                            <div className={`w-max absolute clipped`}>
+                                                <Invoiceimage ref={reff} isLocated={ship?.location ? true : false} languge={true} currentTruck={index + 1} currentQuan={ship?.current_quantity} shippingList={ship} inewInv={invoice} />
+                                            </div>
+                                            <Button onClick={() => downloadImageInvoice('test')}>تنزيل</Button>
+                                        </div>
+                                    })
+                                }
+                            </div>
                         </div>
-                    </div>
-                    :
+                        :
                         <>
                             <table>
                                 <tbody>
                                     <tr>
-                                        <th>نوع البطون</th>
-                                        <th>اجمالي الطلب</th>
-                                        <th>تاريخ الفاتورة</th>
-                                        <th>رقم الفاتورة</th>
+                                        <th className="p-5">نوع البطون</th>
+                                        <th className="p-5">اجمالي الطلب</th>
+                                        <th className="p-5">تاريخ الفاتورة</th>
+                                        <th className="p-5">رقم الفاتورة</th>
                                     </tr>
                                     {
                                         CustomerInvoices?.map((cus) => {
-                                            return <tr onClick={() => { setInvoice(cus); setShowInvoice(true) }}>
-                                                <th>{cus.invoices_kind_type_of_concrete}</th>
-                                                <th>{cus.invoices_quantity}</th>
-                                                <th>{cus.invoices_data}</th>
-                                                <th>{cus.invoices_id}</th>
+                                            return <tr className="cursor-pointer" onClick={() => { setInvoice(cus); setShowInvoice(true) }}>
+                                                <th className="p-5">{cus.invoices_kind_type_of_concrete}</th>
+                                                <th className="p-5">{cus.invoices_quantity}</th>
+                                                <th className="p-5">{cus.invoices_data}</th>
+                                                <th className="p-5">{cus.invoices_id}</th>
                                             </tr>
                                         })
                                     }
